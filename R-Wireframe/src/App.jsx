8f8acd8584d7d9cc7fc6c0adc3a437b63eafcbcf
Josue Armando Uriarte Reyes <47550290+JosueUriarte@users.noreply.github.com>
2023-05-29 19:38:42 -0700

    Some styling for the flowchart

--- R-Wireframe/src/App.jsx
+++ R-Wireframe/src/App.jsx
@@ -19,13 +19,15 @@ const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <>
       <div><h1>R-WireFrame</h1></div>
-      <ReactFlow nodes={initialNodes} edges={initialEdges}>
-        <Background variant="dots" gap={20} size={2} />
-      </ReactFlow>
-      <div><p>created by Josue U. and Miguelcloid R.</p></div>
-    </div>
+      <div style={{ border: '2px solid black', width: '50vw', height: '50vh' }}>
+        <ReactFlow nodes={initialNodes} edges={initialEdges}>
+          <Background variant="dots" gap={20} size={2} />
+        </ReactFlow>
+        <div><p>Created by Josue U. and Miguelcloid R.</p></div>
+      </div>
+    </>
   )
 }
 
