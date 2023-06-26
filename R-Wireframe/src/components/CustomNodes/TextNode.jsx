eb66f84f10845a7d252bdc6af11e566aa09093f2
Miguelcloid Reniva <14044314+Cloid@users.noreply.github.com>
2023-06-26 00:40:15 -0700

    Fixed double click bug

--- R-Wireframe/src/components/CustomNodes/TextNode.jsx
+++ R-Wireframe/src/components/CustomNodes/TextNode.jsx
@@ -1,24 +1,35 @@
-import { useState, useCallback } from "react";
+import { useRef, useCallback } from "react";
 import ContentEditable from "react-contenteditable";
 // https://blixtdev.com/how-to-use-contenteditable-with-react/
 // TODO: look into santizehtml, author says its used for xml attacks
 // TODO: add dynamic logic node name (prob with data)
-// TODO: Fix double click mechanic
+// TODO: Fix clicking on text to cursor mechanic
 function TextNode({ data, isConnectable }) {
-  const [content, setContent] = useState("Sample Text Object");
+  const text = useRef("test");
 
-  const onContentChange = useCallback((evt) => {
-    setContent(evt.currentTarget.innerHTML);
-  }, []);
+  const handleContentChange = (evt) => {
+    text.current = evt.target.value;
+  };
+
+  const handleOnClick = (evt) => {
+    // Check detail if user double click the node
+    if (evt.detail == 2 && window.getSelection) {
+      let selection = window.getSelection();
+      let range = document.createRange();
+      range.selectNodeContents(evt.target);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  };
 
   return (
     <div className="text-updater-node">
       <blockquote>
         <ContentEditable
-          onChange={onContentChange}
-          onBlur={onContentChange}
+          onChange={handleContentChange}
+          onClick={handleOnClick}
           disabled={false}
-          html={content}
+          html={text.current}
         />
       </blockquote>
     </div>
--- R-Wireframe/src/components/CustomNodes/TextObject.jsx
+++ R-Wireframe/src/components/CustomNodes/TextObject.jsx
@@ -1,22 +1,33 @@
-import { useState, useCallback } from "react";
+import { useRef, useCallback } from "react";
 import { Handle, Position } from "reactflow";
 import ContentEditable from "react-contenteditable";
 import "./CustomNodes.css";
 
 function TextObject({ data, isConnectable }) {
-  const [content, setContent] = useState("Sample Text Object");
+  const text = useRef("test");
 
-  const onContentChange = useCallback((evt) => {
-    setContent(evt.currentTarget.innerHTML);
-  }, []);
+  const handleContentChange = (evt) => {
+    text.current = evt.target.value;
+  };
+
+  const handleOnClick = (evt) => {
+    // Check detail if user double click the node
+    if (evt.detail == 2 && window.getSelection) {
+      let selection = window.getSelection();
+      let range = document.createRange();
+      range.selectNodeContents(evt.target);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  };
 
   return (
     <blockquote>
       <ContentEditable
-        onChange={onContentChange}
-        onBlur={onContentChange}
+        onChange={handleContentChange}
+        onClick={handleOnClick}
         disabled={false}
-        html={content}
+        html={text.current}
       />
     </blockquote>
   );
